3c8894977689cd8a57c6b85940d318fcc6aae72f
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-06-06 22:40:43 +0000

    - added more attributes to the Query object to hold the correct answer, KB ID node, and entity type - added code to load the entity type and KB ID node from a file, and fill in in the correct query object

--- TAC-KBP2011/src/parse-queries.py
+++ TAC-KBP2011/src/parse-queries.py
@@ -10,6 +10,7 @@ import sys
 import os
 import re
 import time
+import fileinput
 
 import MySQLdb
 import StringIO 
@@ -17,16 +18,36 @@ import StringIO
 from datetime import datetime
 from BeautifulSoup import BeautifulSoup,NavigableString
 
+queries = {}
+
+
 class Query:
     
     id = None
     string_name = ''
     doc_id = None
+    entity_kb_id = None
+    entity_type = None
     
+
     def __init__(self,identifier,string,document):
         self.id = identifier
         self.string_name = string
         self.doc_id = document
+    
+
+
+def parse_queries_types(file):
+    
+    for line in fileinput.input(file):
+        query_id,kb_id,entity_type = line.split("\t")
+        
+        try:
+            queries[query_id].entity_kb_id = kb_id 
+            queries[query_id].entity_type = entity_type
+            
+        except:
+            print "Error! query: ", query_id, "not parsed"
     
 
 def parse_queries(file):
@@ -43,14 +64,33 @@ def parse_queries(file):
                 docid = item.childNodes[3].childNodes[0].nodeValue
                 
                 query = Query(id,name,docid)
+                queries[id] = query
     
     except:
         print "Error parsing queries"
 
 
+
 def main():
-    
+
+    """    
+    if os.path.isdir(sys.argv[1]):
+        
+        dir_list = os.listdir(sys.argv[1])
+        
+        for file in sorted_dir_list:
+            
+            if file.rsplit(".")[1] == 'xml':
+    """
     parse_queries(sys.argv[1])
-    
+    parse_queries_types(sys.argv[2])
+
+   
+    for q in queries:
+        print queries[q].id, queries[q].string_name, queries[q].doc_id, queries[q].entity_kb_id, queries[q].entity_type, 
+        
 if __name__ == "__main__":
     main()
+    
+
+
