b6a39264ac2b74131d39dcd0788297a7588b97a7
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-07-04 18:43:50 +0000

    - two files added in python (still in development) anlyze-results: to analyze the output of the tunning process for BM25 answer-queries: to parse a query, extracting also information from the support document

---
+++ TAC-KBP2011/src/answer-queries.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Query:
+    
+    id = None
+    string_name = ''
+    doc_id = None
+    entity_kb_id = None
+    entity_type = None
+    
+    def __init__(self,identifier,string,document):
+        self.id = identifier
+        self.string_name = string
+        self.doc_id = document
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
