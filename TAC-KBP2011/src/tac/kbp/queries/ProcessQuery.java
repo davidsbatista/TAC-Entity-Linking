a5a11d46e5e42be24a6ddddbaadd202b9732d3ae
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-07-11 17:25:56 +0000

    - added code to parse the query XML files

--- TAC-KBP2011/src/tac/kbp/queries/ProcessQuery.java
+++ TAC-KBP2011/src/tac/kbp/queries/ProcessQuery.java
@@ -1,5 +1,63 @@
 package tac.kbp.queries;
 
+import java.io.File;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class ProcessQuery {
+	
+	/*
+	 <kbpentlink>
+	 <query id="EL000014">
+	 <name>AZ</name>
+	 <docid>eng-WL-11-174595-12967314</docid>
+	 </query>
+	 */
+	
+	public static void main(String[] args) {
+
+		try {
+
+			File fXmlFile = new File(args[0]);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+			
+			NodeList nList = doc.getElementsByTagName("query");
+			
+			System.out.println(nList.getLength());
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+
+					String query_id = eElement.getAttribute("id");
+					String name = getTagValue("name", eElement);
+					String docid = getTagValue("docid", eElement);
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String getTagValue(String sTag, Element eElement){
+	    NodeList nlList= eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+	    Node nValue = (Node) nlList.item(0); 
+	 
+	    return nValue.getNodeValue();    
+	 }
 
 }
--- TAC-KBP2011/src/tac/kbp/queries/Query.java
+++ TAC-KBP2011/src/tac/kbp/queries/Query.java
@@ -1,7 +1,29 @@
 package tac.kbp.queries;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+
 public class Query {
 	
+	String query_id;
+	String name;
+	String docid;
 	
-
+	
+	public Query(String query_id, String name, String docid) {
+		super();
+		this.query_id = query_id;
+		this.name = name;
+		this.docid = docid;
+	}
+	
 }
