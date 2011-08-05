64c170eeb943f6bf3d53228a4e600de59683236f
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-08-05 18:20:38 +0000

    - query class now has:   NE in support document   context sentences where the entity occurs

--- TAC-KBP2011/src/python-utils/process-queries.py
+++ TAC-KBP2011/src/python-utils/process-queries.py
@@ -3,18 +3,20 @@
 
 import sys
 
-import nltk
-
 import fileinput
 
+import nltk
+
 import xml.dom.minidom
-import urllib
 import httplib
+import urllib
 import simplejson
-import tidy  
+
+from BeautifulSoup import BeautifulSoup
 
 docs_locations = dict()
 queries = []
+
 
 class Query:
     
@@ -24,9 +26,11 @@ class Query:
     entity_kb_id = None
     entity_type = None
     
+    alternative_names = None
     support_doc_persons = None
     support_doc_organizations = None
     support_doc_places = None
+    support_doc_context_occurences = None
 
     def __init__(self,identifier,string,document):
         self.id = identifier
@@ -35,6 +39,8 @@ class Query:
         self.support_doc_persons = xml.dom.minidom.NodeList
         self.support_doc_organizations = xml.dom.minidom.NodeList
         self.support_doc_places = xml.dom.minidom.NodeList
+        self.support_doc_context_occurences = []
+        self.alternative_names = []
 
 
 def load_docs_locations(file):
@@ -103,57 +109,91 @@ def get_entities(query):
     document = file.read()
 
     # do NER on the document
-    rembrandted_document = call_rembrandt(document)
+    rembrandted_document = call_rembrandt(document)    
+    rembrandted_document_valid_xml = BeautifulSoup(rembrandted_document)
     
-    # tidy the xml output by REMBRANDT to make it parsable by xml.dom.minidom.parseString
-    options = { 'output_xhtml' : 1, 'add_xml_decl' : 1, 'indent' : 1, 'input-encoding' : 'utf8', 'output-encoding' : 'utf8', 'tidy_mark' : 0}    
-    xml_tidydoc = tidy.parseString(rembrandted_document.encode("utf8"),**options)
-    
-    #try:
-    print xml_tidydoc
-    
-    xmldoc = xml.dom.minidom.parseString(str(xml_tidydoc))
-    
-    persons = None
-    organizations = None
-    places = None
-                
-    persons = xmldoc.getElementsByTagName('PERSON')
-    organizations = xmldoc.getElementsByTagName('ORGANIZATION')
-    places = xmldoc.getElementsByTagName('PLACE')
+    doc = unicode(rembrandted_document_valid_xml)
+
+    try:
+        xmldoc = xml.dom.minidom.parseString(doc.encode("utf-8"))
         
-    query.support_doc_persons = persons;
-    query.support_doc_organizations = organizations;
-    query.support_doc_places = places;
+        persons = None
+        organizations = None
+        places = None
+                    
+        persons = xmldoc.getElementsByTagName('person')
+        organizations = xmldoc.getElementsByTagName('organization')
+        places = xmldoc.getElementsByTagName('place')
+            
+        query.support_doc_persons = persons;
+        query.support_doc_organizations = organizations;
+        query.support_doc_places = places;
 
-    #except:
-        #print "Error parsing XML returned by REMBRANDT"
-        #print str(xml_tidydoc)
+    except Exception, e:
+        print "Error parsing XML returned by REMBRANDT"
+        print e
+        print rembrandted_document.encode("utf8")
 
 
-def concordance(doc_id):
+def get_context(query):
+    
+    # let's get the document location
+    file = open(docs_locations[query.doc_id]+"/"+query.doc_id+".sgm")
+    document = file.read()
+    
     tokens = nltk.word_tokenize(document)
     text = nltk.Text(tokens)
     concordance = nltk.ConcordanceIndex(text)
-    print "context for: " + '"'+entity+'"'
-    offsets = concordance.offsets(entity)   
-    concordance.print_concordance(entity);
+    
+    """ adapted from nltk.text.ConcordanceIndex.print_concordance """
+    offsets = concordance.offsets(query.string_name)
+    occurrences = []
+    lines=25
+    width=75
+    half_width = (width - len(query.string_name) - 2) / 2 
+    context = width/4 # approx number of words of context
+    
+    if offsets: 
+        lines = min(lines, len(offsets))  
+        for i in offsets: 
+            if lines <= 0: 
+                break 
+            left = (' ' * half_width + ' '.join(tokens[i-context:i])) 
+            right = ' '.join(tokens[i+1:i+context]) 
+            left = left[-half_width:] 
+            right = right[:half_width]                          
+            occurrences.append(left + " " + tokens[i] + " " + right)
+            lines -= 1
+
+        query.support_doc_context_occurences = occurrences
+
+
+def remove_stop_words(sentence):
+    word_list = nltk.word_tokenize(sentence)
+    for word in word_list: # iterate over word_list
+        if word in nltk.corpus.stopwords.words('english'): 
+            word_list.remove(word) # remove word from filtered_word_list if it is a stopword
+    return word_list
 
 
 def analyze_support_document(query):
+    
+    print "Suppor document: ", docs_locations[query.doc_id]+"/"+query.doc_id+".sgm"
+    
     get_entities(query)
-    print query.support_doc_persons
-    print query.support_doc_organizations
-    print query.support_doc_places
-
+    print "PERSONS", len(query.support_doc_persons)
+    print "ORGANIZATIONS", len(query.support_doc_organizations)
+    print "PLACES", len(query.support_doc_places)
+    
+    get_context(query)
+    print "Number context occurence sentences", len(query.support_doc_context_occurences)
+    
 
 def get_alternative_names(query_string):
     """ get alternative names """
 
 
-
-
-def start():    
+def start():
     for q in queries:
         print "Processing query: " + q.id +' "'+ q.string_name +'"' 
         print 
@@ -164,6 +204,8 @@ def start():
         """
         
         analyze_support_document(q)
+        #query_lucene
+        #topics similarity
         
 
 
