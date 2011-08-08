0e9c0f4519cf2ca60e4ac860068efd11fb7dd221
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-08-08 23:47:55 +0000

    - calls GibbsLDA - saves gathered information to disk to be used later

--- TAC-KBP2011/src/python-utils/process-queries.py
+++ TAC-KBP2011/src/python-utils/process-queries.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-
+import os,sys
+import re
 import fileinput
+import string
 
 import nltk
+import MySQLdb
 
 import xml.dom.minidom
 import httplib
@@ -13,10 +15,15 @@ import urllib
 import simplejson
 
 from BeautifulSoup import BeautifulSoup
+from subprocess import Popen, PIPE
 
 docs_locations = dict()
 queries = []
+wordmaps_lda = dict()
 
+conn = MySQLdb.connect(host="agatha.inesc-id.pt", user="publico", passwd="publ1c0", db="wikipedia", charset="utf8", use_unicode=True)
+
+lda_models = "/collections/TAC-2011/lda_models/"
 
 class Query:
     
@@ -42,6 +49,16 @@ class Query:
         self.support_doc_context_occurences = []
         self.alternative_names = []
 
+def remove_tags(data):
+    p = re.compile(r'<.*?>|&quot;')
+    return p.sub('', data)
+
+def parse_doc(document):
+    xmldoc = xml.dom.minidom.parse(document)
+    items = xmldoc.getElementsByTagName('BODY')
+    text = remove_tags(str(items[0].toxml()))    
+    p = re.compile(r'\s+')
+    return p.sub(' ',text)
 
 def load_docs_locations(file):
     
@@ -49,6 +66,12 @@ def load_docs_locations(file):
         parts = line.split(".sgm");
         docs_locations[parts[0].strip()] = parts[1].strip()
 
+def load_lda_wordmaps(file):
+    
+    for line in fileinput.input(file):
+        parts = line.split(" ");
+        if len(parts)>1:
+            wordmaps_lda[parts[1].strip()] = parts[0].strip()
 
 def parse_queries(file):
     
@@ -68,7 +91,6 @@ def parse_queries(file):
     
     except:
         print "Error parsing queries"
-
 
 def call_rembrandt(document):
 
@@ -95,10 +117,13 @@ def call_rembrandt(document):
         
     data = response.read()
     conn.close()
-    JSONObject = simplejson.loads(data)
-
-    return JSONObject["message"]["document"]["body"]
-
+    
+    try:
+        JSONObject = simplejson.loads(data)
+        return JSONObject["message"]["document"]["body"]
+    
+    except Exception, e:
+        print e
 
 def get_entities(query):
     
@@ -106,67 +131,96 @@ def get_entities(query):
     
     # let's get the document location
     file = open(docs_locations[query.doc_id]+"/"+query.doc_id+".sgm")
+    #file = open(docs_location+query.doc_id+".sgm")
     document = file.read()
 
     # do NER on the document
-    rembrandted_document = call_rembrandt(document)    
-    rembrandted_document_valid_xml = BeautifulSoup(rembrandted_document)
+    rembrandted_document = call_rembrandt(document)
     
-    doc = unicode(rembrandted_document_valid_xml)
-
-    try:
-        xmldoc = xml.dom.minidom.parseString(doc.encode("utf-8"))
+    if rembrandted_document:
+          
+        rembrandted_document_valid_xml = BeautifulSoup(rembrandted_document)
         
-        persons = None
-        organizations = None
-        places = None
-                    
-        persons = xmldoc.getElementsByTagName('person')
-        organizations = xmldoc.getElementsByTagName('organization')
-        places = xmldoc.getElementsByTagName('place')
+        doc = unicode(rembrandted_document_valid_xml)
+    
+        try:
+            xmldoc = xml.dom.minidom.parseString(doc.encode("utf-8"))
             
-        query.support_doc_persons = persons;
-        query.support_doc_organizations = organizations;
-        query.support_doc_places = places;
+            persons = None
+            organizations = None
+            places = None
+                        
+            persons = xmldoc.getElementsByTagName('person')
+            organizations = xmldoc.getElementsByTagName('organization')
+            places = xmldoc.getElementsByTagName('place')
+            
+            query.support_doc_persons = getDictWithKeysFromList(persons)
+            query.support_doc_organizations = getDictWithKeysFromList(organizations)
+            query.support_doc_places = getDictWithKeysFromList(places)
+    
+        except Exception, e:
+            print "Error parsing XML returned by REMBRANDT"
+            print e
+            print rembrandted_document.encode("utf8")
+        
+def getDictWithKeysFromList(lst):
+    
+    d = {}
+    
+    for l in lst:
+        
+        try:
+            d[l.firstChild.data.strip().rstrip()] = ""
+        
+        except Exception, e:
+            print e
+    
+    return d
 
-    except Exception, e:
-        print "Error parsing XML returned by REMBRANDT"
-        print e
-        print rembrandted_document.encode("utf8")
+def getConcordance(text,word,offset):
+    
+    tokens = re.findall(u'\w+',text,re.U)
+    listOfIndexes  = list(find_all_in_list( tokens,word))
+    
+    listOfConcordance = []
+    
+    for i in listOfIndexes:
+        
+        start = i - offset
+        
+        if start < 0:
+            start = 0
+        
+        end = i + offset + 1
+         
+        if end > len(text):
+            end = len(text)
+        
+        listOfConcordance.append(tokens[start:end])
+    
+    return listOfConcordance
 
+def find_all_in_list(a_str, sub):
+    
+    start = 0
+    
+    while True:
+         
+        try:
+            start = start + a_str[start:].index(sub)
+        except ValueError:
+            return
+        yield start
+        
+        start+=1
 
 def get_context(query):
     
     # let's get the document location
     file = open(docs_locations[query.doc_id]+"/"+query.doc_id+".sgm")
     document = file.read()
-    
-    tokens = nltk.word_tokenize(document)
-    text = nltk.Text(tokens)
-    concordance = nltk.ConcordanceIndex(text)
-    
-    """ adapted from nltk.text.ConcordanceIndex.print_concordance """
-    offsets = concordance.offsets(query.string_name)
     occurrences = []
-    lines=25
-    width=75
-    half_width = (width - len(query.string_name) - 2) / 2 
-    context = width/4 # approx number of words of context
-    
-    if offsets: 
-        lines = min(lines, len(offsets))  
-        for i in offsets: 
-            if lines <= 0: 
-                break 
-            left = (' ' * half_width + ' '.join(tokens[i-context:i])) 
-            right = ' '.join(tokens[i+1:i+context]) 
-            left = left[-half_width:] 
-            right = right[:half_width]                          
-            occurrences.append(left + " " + tokens[i] + " " + right)
-            lines -= 1
-
-        query.support_doc_context_occurences = occurrences
-
+    query.support_doc_context_occurences = getConcordance(document,query.string_name,10)
 
 def remove_stop_words(sentence):
     word_list = nltk.word_tokenize(sentence)
@@ -175,57 +229,144 @@ def remove_stop_words(sentence):
             word_list.remove(word) # remove word from filtered_word_list if it is a stopword
     return word_list
 
+def get_alternative_names(q):
 
+    SQL = """SELECT en_page.page_title FROM en_redirect, en_page WHERE en_redirect.rd_title = '%s' AND en_redirect.rd_from = en_page.page_id """ % (q.string_name)
+    cursor = conn.cursor()    
+    status = cursor.execute(SQL)
+    rows = cursor.fetchall()
+                
+    redirects = []    
+
+    for r in rows:        
+        redirects.append(r[0])
+    
+    return redirects
+
+def get_topics(query):
+    
+    
+    """ first get the document and transform it for GibbsLDA format:
+        - all words in one line
+        - remove stopwords
+    """
+    
+    file = open(docs_locations[query.doc_id]+"/"+query.doc_id+".sgm")
+    
+    xmldoc = xml.dom.minidom.parse(file)
+    items = xmldoc.getElementsByTagName('BODY')
+    text = remove_tags((items[0].toxml()).encode("utf8"))    
+    p = re.compile(r'\s+')
+    doc_text = p.sub(' ',text)
+    
+    doc_text_no_stopwords = remove_stop_words(doc_text)
+    
+    doc_lda_format = open(lda_models+"/"+query.id+"/"+query.doc_id+'_lda_format', 'w')
+    doc_lda_format.write("1\n"+" ".join(doc_text_no_stopwords))
+    doc_lda_format.close()
+    
+    command = "/home/dsbatista/GibbsLDA++-0.2/src/lda"
+    args = " -inf -dir /collections/TAC-2011/lda_models/ -model model-01000 -dfile " + query.id+"/"+query.doc_id+'_lda_format'
+    
+    print "Calling GibbsLDA++... ", command + args                                         
+    p = Popen(command+args,shell=True,stdout=PIPE,stderr=PIPE)
+    output, stderr_output = p.communicate()
+    print stderr_output     
+    print output
+    
 def analyze_support_document(query):
     
-    print "Suppor document: ", docs_locations[query.doc_id]+"/"+query.doc_id+".sgm"
+    print "Support document: ", docs_locations[query.doc_id]+"/"+query.doc_id+".sgm"
     
     get_entities(query)
-    print "PERSONS", len(query.support_doc_persons)
-    print "ORGANIZATIONS", len(query.support_doc_organizations)
-    print "PLACES", len(query.support_doc_places)
-    
     get_context(query)
+    
+    """ remove stop words from context occurrences """
+    occurences_no_stopwords = []
+    
+    for occ in query.support_doc_context_occurences:
+        occurences_no_stopwords.append(remove_stop_words(" ".join(occ)))
+    
+    query.support_doc_context_occurences = occurences_no_stopwords
+    
     print "Number context occurence sentences", len(query.support_doc_context_occurences)
     
-
-def get_alternative_names(query_string):
-    """ get alternative names """
-
+def query_lucene(q):
+    
+    """
+    print "entity string:", q.string_name
+    print "persons: ", len(q.support_doc_persons)
+    print "organizations: ", len(q.support_doc_organizations)
+    print "places: ", len(q.support_doc_places)
+    print "alternative names: ", q.alternative_names
+    print "context occurences: ", q.support_doc_context_occurences 
+    """
+    
+    try:
+        persons = open(lda_models+"/"+q.id+"/"+'persons.txt', 'w')
+        for p in q.support_doc_persons:
+            persons.write(p.encode("utf8")+"\n")
+        persons.close()
+        
+        organizations = open(lda_models+"/"+q.id+"/"+'organizations.txt', 'w')
+        for o in q.support_doc_organizations:
+            organizations.write(o.encode("utf8")+"\n")
+        organizations.close()
+        
+        places = open(lda_models+"/"+q.id+"/"+'places.txt', 'w')
+        for p in q.support_doc_places:
+            places.write(p.encode("utf8")+"\n")
+        places.close()
+            
+        ocurrences = open(lda_models+"/"+q.id+"/"+'occurrences.txt', 'w')
+        for o in q.support_doc_context_occurences:
+            ocurrences.write((" ".join(o)).encode("utf8")+"\n")
+        ocurrences.close()
+        
+    except Exception, e:
+        print "error in ", q.id
+    
+    """
+    command = "java -jar /collections/TAC-2011/TACKBP.jar"
+    outputdir = q.id
+    args = " "
+    """
 
 def start():
     for q in queries:
-        print "Processing query: " + q.id +' "'+ q.string_name +'"' 
-        print 
+        print "Processing query: " + q.id +' "'+ q.string_name +'"'
         
-        """
-        # get_alternative_names();
-        # query_lucene ();
-        """
+        """ create directory with query_id to store information regarding the query """
+        os.mkdir(lda_models+q.id)
         
+        print "Looking for alternative names"
+        q.alternative_names =  get_alternative_names(q);
         analyze_support_document(q)
-        #query_lucene
+        get_topics(q)
+        query_lucene(q)
         #topics similarity
-        
-
-
+              
 def help():
     print "Usage:"
     print "\t process-queries support-documents-locations-file queries-file"
 
-
-
-
 def main():
-    if len(sys.argv) < 3 or len(sys.argv) > 3:
+    if len(sys.argv) < 4 or len(sys.argv) > 4:
         help()
-    else:  
+    else:
+        print "loading docs locations"
         load_docs_locations(sys.argv[1])
-        parse_queries(sys.argv[2])
-        print len(docs_locations), "support documents loaded"
+        print len(docs_locations), "docs loaded"
+        
+        print "loading LDA wordmaps"
+        load_lda_wordmaps(lda_models+"wordmap.txt")
+        print len(wordmaps_lda), "wordmaps loaded"
+        
+        print "loading queries"
+        parse_queries(sys.argv[3])        
         print len(queries), "queries loaded"
+        print "\n"
         start()
-
 
 
 if __name__ == "__main__":
