bb7decfc1b3b17aa4982cdb3606d6d1296970994
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-09-20 18:48:54 +0000

    - added a normalization procedure, not usefull tought...

--- TAC-KBP2011/src/python-utils/LDA/kullback-leibler-divergence.py
+++ TAC-KBP2011/src/python-utils/LDA/kullback-leibler-divergence.py
@@ -17,15 +17,20 @@ n_candidates = sys.argv[2]
 def main():
     candidates_doc = [[None] * int(n_topics) for i in range(int(n_candidates))]    
     document = []
-    
+
     read_distribution(sys.argv[3],document,False)
+    
+    print document
+    print len(document)
+    
     read_distribution(sys.argv[4],candidates_doc,True)
     
     print "candidates: ", len(candidates_doc)
-    print "document: ", len(document)
+    print "n_topics: ", len(document)
     
     kl_divergence(document, candidates_doc)
-    
+
+
 
 def read_distribution(file,list,candidates):
     i = 0
@@ -44,20 +49,26 @@ def read_distribution(file,list,candidates):
             i+=1
 
 
-def kl_divergence(q,p):
-    lowest = 10
+
+def kl_divergence(p,q):
+    lowest = 1
     topic = -1
     for z in range(int(n_candidates)):
         sum = 0
         for i in range(int(n_topics)):
-            sum += p[z][i] * math.log10((p[z][i]/q[i]))
+            sum += p[i] * ( math.log10(p[i]) - math.log10(q[z][i]) )
+            print p[i], q[z][i], sum
+        
         if sum <= lowest:
-            print sum
             lowest = sum
             topic = z    
-        print z,sum
     
-    print "closest candidate topics distribuition: ", topic, lowest
+    """
+    normalization: 1−exp(−Divergence K-L)
+    normalized = 1 - math.exp(lowest-lowest*2)
+    """
+    
+    print "divergence: ", lowest 
 
 
 if __name__ == "__main__":
