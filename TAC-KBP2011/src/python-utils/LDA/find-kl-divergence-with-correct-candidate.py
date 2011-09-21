566639714366dccf177e8d857ad7e5d4984961f3
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-09-21 13:15:36 +0000

    - added code to calculate divergence with a random entity from the KB

--- TAC-KBP2011/src/python-utils/LDA/find-kl-divergence-with-correct-candidate.py
+++ TAC-KBP2011/src/python-utils/LDA/find-kl-divergence-with-correct-candidate.py
@@ -5,11 +5,15 @@ import os
 import sys
 import fileinput
 import math
+import random
 
 from subprocess import Popen, PIPE
 
 n_topics = 200
+num_entities = 818741
+
 gold_standard = []
+
 
 def copy_data():
     for line in fileinput.input(sys.argv[1]):
@@ -22,7 +26,7 @@ def copy_data():
         get the topics distribution for the correct_candiate:
         it's the big file: KB_one_file_documents_rare_words_removed.txt.theta
         the line number in the file corresponds to the entity ID 
-        
+        """
     
         command = "head -n " + id + " /collections/TAC-2011/lda_distribution_knowledge_base/KB_one_file_documents_rare_words_removed.txt.theta | tail -n 1"
         p = Popen(command,shell=True,stdout=PIPE,stderr=PIPE)
@@ -32,8 +36,7 @@ def copy_data():
         f = open(id+".theta", 'w')
         f.write(output)
         f.close()
-        """
-
+        
         """
         get the topics distributions for the query document: 
         (.theta file in the "/collections/TAC-2011/lda_distribution_queries/parts[0]" directory)
@@ -43,6 +46,7 @@ def copy_data():
         command = "mkdir queries/" + query + ";cp " + dir + "/*.theta " + "queries/" + query + "/"
         p = Popen(command,shell=True,stdout=PIPE,stderr=PIPE)
 
+
 def load_gold_standard():
     for line in fileinput.input(sys.argv[1]):
         parts = line.split("\t")
@@ -50,7 +54,9 @@ def load_gold_standard():
         id = parts[1].split("E")[1].strip()        
         gold_standard.append((query,id))
 
-def calculate_divergence():
+
+def calculate_divergence_gold_standard():
+    
     for e in gold_standard:        
         query = e[0]        
         id = e[1]
@@ -69,7 +75,45 @@ def calculate_divergence():
 
         divergence = kl_divergence(correct, document)
         
-        print query, divergence
+        print query, id, divergence
+
+
+def calculate_divergence_random_entity():
+    for e in gold_standard:
+        """
+        for each query, get the topics distribution for an entity from the KB which is not the correct one
+        """
+        query = e[0]
+        random_entity = int(random.random()*num_entities)
+        while random_entity == int(e[1]):
+            random_entity = int(random.random()*num_entities)
+        
+        command = "head -n " + str(random_entity) + " /collections/TAC-2011/lda_distribution_knowledge_base/KB_one_file_documents_rare_words_removed.txt.theta | tail -n 1"
+        p = Popen(command,shell=True,stdout=PIPE,stderr=PIPE)
+        output, stderr_output = p.communicate()
+        
+        kb_entity = []        
+ 
+        topics_disturbution =  output.rstrip().split(" ")
+        for d in topics_disturbution:
+            kb_entity.append(float(d))
+                    
+        """
+        get the topics distribution for the query's document support
+        """
+        document_dir = "/collections/TAC-2011/find-kl-divergence-with-correct-candidate/queries/"+ query + "/"
+        dirList = os.listdir(document_dir)
+        document_file = document_dir + dirList[0]
+        document = []
+        
+        read_distribution(document_file,document)
+    
+        """
+        calculate the KL-Divergence
+        """
+        divergence = kl_divergence(document,kb_entity)
+        print query, random_entity, divergence 
+    
 
 def read_distribution(data,list):
     i = 0
@@ -78,16 +122,19 @@ def read_distribution(data,list):
         for d in topics_disturbution:
             list.append(float(d))
 
+
 def kl_divergence(p,q):
     divergence = 0.0
     for i in range(int(n_topics)):
         divergence += p[i] * ( math.log10(p[i]) - math.log10(q[i]) )
     return divergence
+ 
        
 def main():
     #copy_data()
-    load_gold_standard()    
-    calculate_divergence()
+    load_gold_standard()
+    #calculate_divergence_gold_standard()
+    calculate_divergence_random_entity()
 
 if __name__ == "__main__":
     main()
