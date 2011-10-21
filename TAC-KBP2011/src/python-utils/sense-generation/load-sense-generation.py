2617194969b7f69a878c2dab0ee69cb26383607e
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-10-21 18:01:03 +0000

    - loads all sense generation data into a redis distributed hash-table

--- TAC-KBP2011/src/python-utils/sense-generation/load-sense-generation.py
+++ TAC-KBP2011/src/python-utils/sense-generation/load-sense-generation.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import pickle
 import redis
 import fileinput
 
+big_dict = dict()
 acronyms = dict()
 
 r = redis.Redis(host='localhost', port=6379, db=0)
@@ -15,7 +15,6 @@ def read(file):
     mappings = pickle.load(pkl_file)
     return mappings
     pkl_file.close()
-
 
 def load_acronyms(file):
     
@@ -29,49 +28,63 @@ def load_acronyms(file):
             acronyms[acronym].append(expansion)
               
         except:
-            acronyms[acronym] = list()
+            acronyms[acronym] = []
             acronyms[acronym].append(expansion)
 
 def load(dict):
     h_keys = dict.keys()
     
-    """
-    http://abaff.wordpress.com/2009/05/22/memcached-keys-cant-have-spaces-in-them/:
-    keys cannot have spaces in them!
-    """
+    for k in h_keys:        
+
+        try:
+            for el in dict[k]:
+                big_dict[k].append(el)
+                                  
+        except:
+            big_dict[k] = []
+            for el in dict[k]:
+                big_dict[k].append(el)
+
+def load_to_cache():
+    h_keys = big_dict.keys()
     
     for k in h_keys:
-        
-        try:
-            r.set(k, dict[k])
-                      
-        except Exception, e:
-            print e
+        r.set(k,big_dict[k])
+
 
 def main():
-    
+
     print "loading disambiguation"
-    disambiguation = pickle.load(open("disambiguation_pages.pkl"))
+    disambiguation = eval(open("disambiguation.file").read())
     print "nº disambiguation entries to load: ", len(disambiguation)
-    load (disambiguation)
-    print r.info
+    load(disambiguation)
+    disambiguation = dict()
     
     print "loading normalized"
-    normalized = pickle.load(open('normalized_articles_and_redirects.pkl'))
+    normalized = eval(open("normalized.file").read())
     print "nº normalized entries to load: ", len(normalized)
-    load (normalized)
-    print r.info    
+    load(normalized)    
+    normalized = dict() 
     
     print "loading anchors"
-    anchors = pickle.load(open("anchors_to_title_page.pkl"))
-    print "nº anchors entries to load: ", len(normalized)
+    anchors = eval(open("anchors.file").read())
+    print "nº anchors entries to load: ", len(anchors)
     load(anchors)
-    print r.info
+    anchors = dict()
     
-    print "loading anchors"
+    print "load acronyms"
     load_acronyms(sys.argv[1])
+    #print "nº acronyms entries to load: ", len(acronyms)
     load(acronyms)
+    acronyms = dict()
     
+    print "writing to file"
+    f = open("all.file","w")
+    f.write(str(big_dict))
+    f.close()
+    
+    print "loading to redis server"
+    load_to_cache()
     
 if __name__ == "__main__":
     main()
