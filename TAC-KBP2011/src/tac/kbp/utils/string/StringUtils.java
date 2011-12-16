f7916932cc11d627a620e2851c4b9ca4fadd5bcf
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2011-12-16 21:31:31 +0000

    - refactoring

--- TAC-KBP2011/src/tac/kbp/utils/string/StringUtils.java
+++ TAC-KBP2011/src/tac/kbp/utils/string/StringUtils.java
@@ -1,5 +1,16 @@
 package tac.kbp.utils.string;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+
+import tac.kbp.bin.Definitions;
+
 public class StringUtils {
 	
 	public static boolean isUpper(String s) {
@@ -9,6 +20,35 @@ public class StringUtils {
 	            return false;
 	    }
 	   return true;
+	}
+	
+	public static Map<String, Integer> tokenized(String s) throws IOException {
+    	
+    	WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer();
+    	
+    	Map<String, Integer> result = new HashMap<String, Integer>();
+    	TokenStream stream  = analyzer.tokenStream(null, new StringReader(s));
+    	//OffsetAttribute offsetAttribute = stream.getAttribute(OffsetAttribute.class);
+		TermAttribute termAttribute = stream.getAttribute(TermAttribute.class);
+    	String term = new String();
+		
+		while (stream.incrementToken()) {
+    		//int startOffset = offsetAttribute.startOffset();
+    		//int endOffset = offsetAttribute.endOffset();
+    		term = termAttribute.term();
+    		
+    		// don't include stopwords
+    		if (Definitions.stop_words.contains(term)) {
+    			continue;
+			}
+    		try {
+    			int value = result.get(term);
+    			result.put(term, value + 1);
+    		} catch (NullPointerException e) {
+    			result.put(term, 1);
+    		}
+		}
+		return result;
 	}
 	
 	public static String removeTags(String text) { 
