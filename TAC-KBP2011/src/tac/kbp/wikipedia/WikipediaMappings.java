dc167f83f5c8d8b4897dfc866ae70e12e950145d
dsbatista <dsbatista@8ddc6f7c-6f37-31f7-3f12-7dfd943d54fb>
2012-03-12 19:40:45 +0000

    - mappings using JWPL

--- TAC-KBP2011/src/tac/kbp/wikipedia/WikipediaMappings.java
+++ TAC-KBP2011/src/tac/kbp/wikipedia/WikipediaMappings.java
@@ -1,26 +1,37 @@
 package tac.kbp.wikipedia;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.LinkedHashMultimap;
+
 import de.tudarmstadt.ukp.wikipedia.api.DatabaseConfiguration;
 import de.tudarmstadt.ukp.wikipedia.api.Page;
+import de.tudarmstadt.ukp.wikipedia.api.PageQuery;
 import de.tudarmstadt.ukp.wikipedia.api.WikiConstants.Language;
 import de.tudarmstadt.ukp.wikipedia.api.Wikipedia;
 import de.tudarmstadt.ukp.wikipedia.api.exception.WikiApiException;
+import de.tudarmstadt.ukp.wikipedia.api.exception.WikiTitleParsingException;
+import de.tudarmstadt.ukp.wikipedia.parser.Link;
+import de.tudarmstadt.ukp.wikipedia.parser.NestedListContainer;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
 
 public class WikipediaMappings {
 	
 	public static DatabaseConfiguration dbConfig;
 	public static Wikipedia wiki;
-	
 	public static Map<String,String> redirectsAndNormalized = new HashMap<String, String>();
+	public static LinkedHashMultimap<String,String> hyperlinks; 
 	
 	public static void init(String host, String database, String user, String password) throws WikiApiException {
 		
@@ -38,70 +49,46 @@ public class WikipediaMappings {
 		System.out.println(wiki.getWikipediaId());
 	}
 	
-	public static void getAllPages() throws WikiApiException {
+	public static void getArticlesLink() throws WikiTitleParsingException, IOException {
+		
+		hyperlinks = com.google.common.collect.LinkedHashMultimap.create();
 		
 		for (Page page : wiki.getArticles()) {
-		    getInfo(page.getTitle().getWikiStyleTitle());
-		}		
-	}
-		
-	public static void getInfo(String title) throws WikiApiException {
-
-		Page page = wiki.getPage(title);
-		        
-		// the title of the page
-		System.out.println("Queried string       : " + title);
-		System.out.println("Title                : " + page.getTitle());
-
-		// whether the page is a disambiguation page
-		System.out.println("IsDisambiguationPage : " + page.isDisambiguation());
-		        
-		// whether the page is a redirect
-		// If a page is a redirect, we can use it like a normal page.
-		// The other infos in this example are transparently served by the page that the redirect points to. 
-		System.out.println("redirect page query  : " + page.isRedirect());
-		
-		/*
-		// the number of links pointing to this page
-		System.out.println("# of ingoing links   : " + page.getNumberOfInlinks());
-		*/
-		        
-		// the number of links in this page pointing to other pages
-		System.out.println("# of outgoing links  : " + page.getNumberOfOutlinks());
-
-		// the number of categories that are assigned to this page
-		System.out.println("# of categories      : " + page.getNumberOfCategories());
-		
-		//System.out.println("text: " + "\n" + page.getPlainText());
-		
-		/*
-		System.out.println("inLinkAnchors");
-		Set<String> inLinkAnchors = page.getInlinkAnchors();
-		
-		for (String string : inLinkAnchors) {
-			System.out.println(string);
-		}
-		*/
-		
-		Set<String> getRedirects = page.getRedirects();
-		
-		System.out.println("Redirects");
-		for (String string : getRedirects) {
-			System.out.println(string);
-		}
-
-		/*
-		System.out.println("Outlinks");
-		Map<String,Set<String>> outlinkanchors = page.getOutlinkAnchors();
-		Set<Page> inlinks = page.getInlinks();
-		}
-		*/
-	}
-		
-	public static void getRedirects() throws WikiApiException, IOException {
+			
+			ParsedPage p;
+			
+			try {
 				
+				p = page.getParsedPage();
+				
+				for (Link link : p.getLinks()) { 
+					if (link.getType() == Link.type.INTERNAL) {
+						String text = link.getText();
+						String target = link.getTarget(); 
+						if (!text.equalsIgnoreCase(target.replaceAll("_", " ")) && text.length()>0) {
+							System.out.println(link.getText().toLowerCase().replaceAll("_", " ") + "->" + link.getTarget().replaceAll("#.*", ""));
+							String text_normalized = link.getText().toLowerCase().replaceAll("_", " ");
+							String target_parsed = link.getTarget().replaceAll("#.*", "");
+							hyperlinks.put(text_normalized, target_parsed);
+							System.out.println(hyperlinks.size());
+						}
+					}
+				}
+			
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
-		
+		FileOutputStream fos = new FileOutputStream("hyperlinks");
+	    ObjectOutputStream oos = new ObjectOutputStream(fos);
+	    oos.writeObject(hyperlinks);
+	    oos.close();
+	}
+	
+	public static void getRedirects() throws WikiApiException, IOException {
+
 		for (Page page : wiki.getArticles()) {
 			
 		    Set<String> redirects = page.getRedirects();
@@ -125,6 +112,28 @@ public class WikipediaMappings {
 	}
 
 	@SuppressWarnings("unchecked")
+	public static void loadArticlesLinks(String filename) throws IOException, ClassNotFoundException {
+		
+		hyperlinks = com.google.common.collect.LinkedHashMultimap.create();
+		
+	    FileInputStream fis = new FileInputStream(filename);
+	    ObjectInputStream ois = new ObjectInputStream(fis);
+	    hyperlinks = (LinkedHashMultimap<String, String>) ois.readObject();
+	    ois.close();
+	    
+	    
+	    Set<String> keys = hyperlinks.keySet();
+	    
+	    for (String k : keys) {
+			Set<String> links = hyperlinks.get(k);
+			System.out.println(k);
+			for (String l : links) {
+				System.out.println('\t'+l);
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
 	public static void loadRedirects(String filename) throws IOException, ClassNotFoundException {
 		
 	    FileInputStream fis = new FileInputStream(filename);
@@ -133,8 +142,37 @@ public class WikipediaMappings {
 	    ois.close();
 	
 	}
-    	
-	public static void main(String args[]) throws WikiApiException, IOException {
+    
+	public static void getDisambiguations() throws WikiApiException {
+		
+		PageQuery pq = new PageQuery();		
+		pq.setOnlyDisambiguationPages(true);
+		
+		for (Page p : wiki.getPages(pq)) {
+			for (NestedListContainer list : p.getParsedPage().getNestedLists()) {
+				for (Link link : list.getLinks()) {
+					System.out.println(link);
+				}
+			}		
+		}
+	}
+
+	public static void getEntities() throws WikiApiException, IOException {
+		
+		PageQuery pq = new PageQuery();				
+		pq.setOnlyArticlePages(true);
+		
+		FileWriter fstream = new FileWriter("entities.txt");
+		BufferedWriter out = new BufferedWriter(fstream);
+		
+		for (Page p : wiki.getPages(pq)) {
+			out.write(p.getTitle().getPlainTitle()+'\n');
+		}
+		out.close();
+	}
+	
+	
+	public static void main(String args[]) throws WikiApiException, IOException, ClassNotFoundException {
 		
 		String host = args[0];
 		String database = args[1];
@@ -142,7 +180,12 @@ public class WikipediaMappings {
 		String passwd = args[3];
 		
 		init(host, database, user, passwd);
-		getRedirects();		
+		//getRedirects();	
+		//loadRedirects(args[4]);
+		getArticlesLink();
+		//loadArticlesLinks(args[4]);
+		//getDisambiguations();
+		//getEntities();
 	}
 }
 
